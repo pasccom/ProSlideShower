c8536c58ad85f77efb039258f957d86edab137cb
Pascal COMBES <pascom@orange.fr>
2016-09-04 18:56:01 +0200

    The translation files finished.

---
+++ proslideshower_en.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>ProjControllerPane</name>
+    <message>
+        <location filename="projcontrollerpane.cpp" line="28"/>
+        <source>Open master</source>
+        <translation>Open a master document</translation>
+    </message>
+    <message>
+        <location filename="projcontrollerpane.cpp" line="33"/>
+        <source>Configure</source>
+        <translation>Configure</translation>
+    </message>
+    <message>
+        <location filename="projcontrollerpane.cpp" line="38"/>
+        <source>Quit</source>
+        <translation>Quit</translation>
+    </message>
+</context>
+<context>
+    <name>SubDisplayHandler</name>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="62"/>
+        <source>Choose a PDF file</source>
+        <translation>Choose a PDF file</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="62"/>
+        <source>PDF files (*.pdf)</source>
+        <translation>PDF files (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="93"/>
+        <source>Number of virtual screens</source>
+        <translation>Number of virtual screens</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="94"/>
+        <source>Enter number of virtual screens in the presentation</source>
+        <translation>Enter number of virtual screens in the presentation</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="131"/>
+        <source>Page offset</source>
+        <translation>Offset</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="132"/>
+        <source>Enter the offset of the displayed page</source>
+        <translation>Enter the offset of the display</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="159"/>
+        <source>Load &amp;Master</source>
+        <translation>Open a &amp;master document</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="161"/>
+        <source>Load &amp;Slave</source>
+        <translation>Open a &amp;slave document</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="163"/>
+        <source>&amp;Virtual screen number</source>
+        <translation>&amp;Virtual screen number</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="165"/>
+        <source>&amp;Display offset</source>
+        <translation>&amp;Display offset</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="189"/>
+        <source>Use virtual screen &amp;%1</source>
+        <translation>Use virtual screen &amp;%1</translation>
+    </message>
+</context>
+</TS>
---
+++ proslideshower_fr.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>ProjControllerPane</name>
+    <message>
+        <location filename="projcontrollerpane.cpp" line="28"/>
+        <source>Open master</source>
+        <translation>Ouvrir un document maître</translation>
+    </message>
+    <message>
+        <location filename="projcontrollerpane.cpp" line="33"/>
+        <source>Configure</source>
+        <translation>Configuration</translation>
+    </message>
+    <message>
+        <location filename="projcontrollerpane.cpp" line="38"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+</context>
+<context>
+    <name>SubDisplayHandler</name>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="62"/>
+        <source>Choose a PDF file</source>
+        <translation>Choisissez un ficher PDF</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="62"/>
+        <source>PDF files (*.pdf)</source>
+        <translation>Fichiers PDF (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="93"/>
+        <source>Number of virtual screens</source>
+        <translation>Nombre d&apos;écrans virtuels</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="94"/>
+        <source>Enter number of virtual screens in the presentation</source>
+        <translation>Entrez le nombre d&apos;écrans virtuels dans la présentation</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="131"/>
+        <source>Page offset</source>
+        <translation>Décalage</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="132"/>
+        <source>Enter the offset of the displayed page</source>
+        <translation>Entrez le décalage de la vue</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="159"/>
+        <source>Load &amp;Master</source>
+        <translation>Ouvrir un document &amp;maître</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="161"/>
+        <source>Load &amp;Slave</source>
+        <translation>Ouvrir un document e&amp;sclave</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="163"/>
+        <source>&amp;Virtual screen number</source>
+        <translation>Nombre d&apos;écrans &amp;virtuels</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="165"/>
+        <source>&amp;Display offset</source>
+        <translation>&amp;Décalage de l&apos;affichage</translation>
+    </message>
+    <message>
+        <location filename="subdisplayhandler.cpp" line="189"/>
+        <source>Use virtual screen &amp;%1</source>
+        <translation>Utiliser l&apos;écran virtuel n°&amp;%1</translation>
+    </message>
+</context>
+</TS>
