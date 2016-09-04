cd6c15367e261555604b6b86f5e1a22f6444e159
Pascal COMBES <pascom@orange.fr>
2016-09-04 18:56:19 +0200

    Load a translation corresponding to system language.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,32 @@
 #include "projmanager.h"
 
+#include <QTranslator>
 #include <QApplication>
+
+#include <QtDebug>
 
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     app.setOrganizationName("Pascom");
     app.setApplicationName("ProSlideShower");
+
+    QTranslator translator(NULL);
+    foreach (QString lang, QLocale::system().uiLanguages()) {
+        QString qmFile = QString(QLatin1String("proslideshower_%1.qm")).arg(lang);
+        if (translator.load(qmFile, ".."))
+            break;
+        if (lang.length() > 2) {
+            qmFile = QString(QLatin1String("proslideshower_%1.qm")).arg(lang.left(2));
+            if (translator.load(qmFile, ".."))
+                break;
+        }
+    }
+
+    if (!translator.isEmpty())
+        app.installTranslator(&translator);
+    else
+        qWarning() << "Could not find a translator.";
 
     ProjManager manager(NULL);
     Q_UNUSED(manager);
