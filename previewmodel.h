62b65c162d85d90e5669e27bd9afc8400b8a5018
Pascal COMBES <pascom@orange.fr>
2016-09-28 20:59:57 +0200

    Documentation for PreviewModel.

--- previewmodel.h
+++ previewmodel.h
@@ -7,15 +7,49 @@
 #include <QSize>
 #include <QtDebug>
 
+/*!
+ * \brief The PreviewModel class is a model for GallleryView.
+ *
+ * This class implements a model suitable for GalleryView.
+ * It only defines a constraint for the horizontal or vertical size
+ * of the preview miniature (returned when PreviewRole is used).
+ */
 class PreviewModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
-    enum GalleryItemDataRole {PreviewRole = Qt::UserRole};
+    /*!
+     * \brief Special roles
+     *
+     * Special roles used by the GalleryView,
+     * that a PreviewModel should implement.
+     */
+    enum {
+        /*!
+         * \brief Role for miniature
+         *
+         * When this role is used, the model should return an image
+         * respecting the preview constraint (see setPreviewConstraint()).
+         */
+        PreviewRole = Qt::UserRole
+   };
 
+    /*!
+     * \brief Constructor
+     *
+     * Only call the parent constructor.
+     * \param parent The parent object.
+     */
     inline PreviewModel(QObject *parent = 0) :
         QAbstractListModel(parent) {}
 
+    /*!
+     * \brief Set the preview constraint
+     *
+     * Set the preview constraint and its orientation (the constrained dimension)
+     * \param dim The value of the constraint (in pixels)
+     * \param orientation The constrained dimension (<tt>Qt::Vertical</tt> for the height, <tt>Qt::Horizontal</tt> for the width).
+     */
     virtual inline void setPreviewConstraint(int dim, Qt::Orientation orientation = Qt::Vertical)
     {
         mConstraintDim = dim;
@@ -23,8 +57,8 @@ public:
         qDebug() << "Constraint: " << dim;
     }
 protected:
-    int mConstraintDim;
-    Qt::Orientation mConstraintOrientation;
+    int mConstraintDim;                     /*!< \internal The value of the constraint */
+    Qt::Orientation mConstraintOrientation; /*!< \internal The constrained dimension (<tt>Qt::Vertical</tt> for the height, <tt>Qt::Horizontal</tt> for the width). */
 };
 
 #endif // GALLERYMODEL_H
