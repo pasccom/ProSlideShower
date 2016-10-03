c4135ef3fd6487becc3b0a06ce41248e60e3b3c5
Pascal COMBES <pascom@orange.fr>
2016-10-03 20:13:20 +0200

    Added licensing information

--- galleryview.h
+++ galleryview.h
@@ -1,5 +1,23 @@
-#ifndef FOLDERVIEWER_H
-#define FOLDERVIEWER_H
+/* Copyright 2016 Pascal COMBES <pascom@orange.fr>
+ * 
+ * This file is part of ProSlideShower.
+ * 
+ * ProSlideShower is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ProSlideShower is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ProSlideShower. If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef GALLERYVIEW_H
+#define GALLERYVIEW_H
 
 //#include <QAbstractScrollArea>
 #include <QAbstractItemView>
@@ -413,4 +431,4 @@ private:
     static const unsigned int SLIDER_SINGLE_STEP = 10;
 };
 
-#endif // FOLDERVIEWER_H
+#endif // GALLERYVIEW_H
